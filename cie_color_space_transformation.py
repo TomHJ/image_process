8f6750af6b9b51129376fda4db4136784b379c1c
TomHJ <645924656@qq.com>
2021-05-25 15:17:25 +0800

    add Lab value

--- cie_color_space_transformation.py
+++ cie_color_space_transformation.py
@@ -8,6 +8,7 @@ sRGB_RGB2XYZ = np.array([[0.4124, 0.3576, 0.1805],
                          [0.0193, 0.1192, 0.9505]])
 
 def XYZ2Lab(XYZ):
+    XYZ=XYZ.reshape((3,))
     XYZ[0] = float(XYZ[0]) / 95.047  # ref_X =  95.047   Observer= 2°, Illuminant= D65
     XYZ[1] = float(XYZ[1]) / 100.0  # ref_Y = 100.000
     XYZ[2] = float(XYZ[2]) / 108.883  # ref_Z = 108.883
@@ -44,9 +45,14 @@ def sRGB_rgb2xyz(inputColor,matrix):
        num = num + 1
    XYZ=np.round(np.matmul(matrix,RGB),decimals=4)
 
-   return XYZ
+   return RGB,XYZ
 
 if __name__=="__main__":
-    r=[255,255,255]
-    XYZ=sRGB_rgb2xyz(r,sRGB_RGB2XYZ)
-    print(XYZ)
+    # sRGB gamut:
+    r=[255,0,0]
+    triRGB,XYZ=sRGB_rgb2xyz(r,sRGB_RGB2XYZ)
+    Lab=XYZ2Lab(XYZ)
+    print("Pixel value:"+str(r))
+    print("Tristimulus RGB value:"+str(triRGB.transpose()[0]))
+    print("Tristimulus XYZ value:"+str(XYZ.transpose()[0]))
+    print("Lab vale:"+str(Lab))
